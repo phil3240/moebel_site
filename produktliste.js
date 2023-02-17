0bfc24954453e88f797e297ce236cbf47804923f
Phillip Kongstad <phil3240@stud.kea.dk>
2023-02-17 14:11:56 +0100

    Et eller andet

--- produktliste.js
+++ produktliste.js
@@ -3,19 +3,20 @@ const kat = urlParams.get("kat");
 const url = "https://moebelsite-f868.restdb.io/rest/moebler";
 
 const options = {
-    headers: {
-        `x-apikey`: "63ee4ec4478852088da68376"
-    }
+  headers: {
+    "x-apikey": "63ee4ec4478852088da68376",
+  },
 };
 
 //document.querySelector("h2").textContent = kat;
-
 
 async function getData() {
   const response = await fetch(url, options);
   const data = await response.json();
   data.forEach(showProduct);
+  console.log(data);
 }
+
 getData();
 
 function showProduct(product) {
@@ -25,16 +26,17 @@ function showProduct(product) {
   copy.querySelector("h3").textContent = product.navn;
   copy.querySelector("#procent").textContent = product.tilbud;
   copy.querySelector(".price").textContent = product.pris;
-  //copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  copy.querySelector("img").src = product.billede;
   copy.querySelector("a").href = "produkt.html?id=" + product.id;
   copy.querySelector(".discounted p span").textContent = Math.round(product.pris - (product.pris * product.tilbud) / 100);
 
-  if (product.soldout) {
-    copy.querySelector("article").classList.add("soldOut");
+  if (product.udsolgt) {
+    copy.querySelector("article").classList.add("udsolgt");
   }
-  if (product.discount) {
-    copy.querySelector("article").classList.add("onSale");
+  if (product.tilbud) {
+    copy.querySelector("article").classList.add("tilbud");
   }
 
   document.querySelector("main").appendChild(copy);
 }
+("billeder/");
